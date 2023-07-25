79bef4dac21175fe88df72bb3902148f56b1875d
Samuel Gebrehiwet <samuel.asmelash2017@gmail.comgit config --global user.name samuel>
2023-07-25 03:02:26 +0000

    commiting new assignment

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+// assertEqual.js
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp"); // ❌ Assertion Failed: Lighthouse Labs !== Bootcamp
+assertEqual(1, 1); // ✅ Assertion Passed: 1 === 1
