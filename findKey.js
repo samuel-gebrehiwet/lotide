a6eb1092a71cd1c10abffa3eb0b00e5392d48068
Samuel Gebrehiwet <samuel.asmelash2017@gmail.comgit config --global user.name samuel>
2023-08-05 09:39:06 +0000

    implementing findKey.js

---
+++ findKey.js
@@ -0,0 +1,36 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  for (const key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+// Test cases
+const restaurants = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+};
+
+const result1 = findKey(restaurants, x => x.stars === 2);
+assertEqual(result1, "noma");
+
+const result2 = findKey(restaurants, x => x.stars === 3);
+assertEqual(result2, "Akaleri");
+
+const result3 = findKey(restaurants, x => x.stars === 4);
+assertEqual(result3, undefined);
+
