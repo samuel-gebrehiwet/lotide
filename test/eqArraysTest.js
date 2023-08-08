742eda318e27be182f2d3928697cf4050287bb5c
Samuel Gebrehiwet <samuel.asmelash2017@gmail.comgit config --global user.name samuel>
2023-08-08 20:37:27 +0000

    refactored eqArrays.js

---
+++ test/eqArraysTest.js
@@ -0,0 +1,10 @@
+// lotide/test/eqArraysTest.js
+const eqArrays = require('../eqArrays');
+const assertEqual = require('../assertEqual');
+
+// Test cases for eqArrays function
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+
