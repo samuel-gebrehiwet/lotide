2c270b9ed9612429c12a8f4926636ee42deb1096
Samuel Gebrehiwet <samuel.asmelash2017@gmail.comgit config --global user.name samuel>
2023-08-08 22:13:57 +0000

    refactore mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -7,3 +7,23 @@ assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // Should pass
 assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // Should pass
 assertArraysEqual(middle([1]), []); // Should pass
 
+const assert = require('chai').assert;
+const middle = require('../middle');
+
+describe("#middle", () => {
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  
+  it("returns [] for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it("returns ['b'] for ['a', 'b', 'c']", () => {
+    assert.deepEqual(middle(['a', 'b', 'c']), ['b']);
+  });
+
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -29,4 +29,20 @@ const words = ["Yo Yo", "Lighthouse", "Labs"];
 const tailResult = tail(words);
 console.log(tailResult); // ["Lighthouse", "Labs"]
 
+const assert = require('chai').assert;
+const tail = require('../tail');
+
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  
+  it("returns [] for [1]", () => {
+    assert.deepEqual(tail([1]), []);
+  });
+
+  it("returns ['b', 'c'] for ['a', 'b', 'c']", () => {
+    assert.deepEqual(tail(['a', 'b', 'c']), ['b', 'c']);
+  });
+});
 
