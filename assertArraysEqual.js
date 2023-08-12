01a783333281b66f050fd7eebe4e265bf901a6cc
Samuel Gebrehiwet <samuel.asmelash2017@gmail.comgit config --global user.name samuel>
2023-08-12 21:37:14 +0000

    refactore and implemnting

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,7 +1,7 @@
 const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function (array1, array2) {
-  if (eqArrays(array1, array2)) {
+  if (eqArrays(array1, array2)){
    console.log(`✅ Assertion Passed: [${array1}] === [${array2}]`);
   } else {
    console.log(`❌ Assertion Failed: [${array1}] !== [${array2}]`);
