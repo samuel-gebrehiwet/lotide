9473e0bc6ce4669768a92519bb5ab3eef49cd041
Samuel Gebrehiwet <samuel.asmelash2017@gmail.comgit config --global user.name samuel>
2023-08-13 14:31:59 +0000

    refactore after reviewing by Matthew Tran

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,20 +1,10 @@
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
-const eqObjects = require("./eqObjects");
+const eqObjects = require('./eqObjects')
 
-// FUNCTION IMPLEMENTATION
 const assertObjectsEqual = function (actual, expected) {
-  // Implement me!
-  const inspect = require("util").inspect; // <= add this line
-
   if (eqObjects(actual, expected)) {
-    console.log(
-      `\u2705 Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`
-    );
+    console.log(`✅ Assertion Passed: Objects are equal`);
   } else {
-    console.log(
-      `\u26D4 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`
-    );
+    console.log(`🛑 Assertion Failed: Objects are not equal`);
   }
 };
 
